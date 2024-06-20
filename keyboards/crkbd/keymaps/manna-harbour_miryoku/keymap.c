4165a3f2c595f8f8c28fec881eeb9f098839b706
lmquansgt <lmquan.sgt@gmail.com>
2024-06-20 13:04:43 +0700

    add crkbd oled

--- keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/keymap.c
@@ -1,4 +1,58 @@
-// Copyright 2019 Manna Harbour
-// https://github.com/manna-harbour/miryoku
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+Copyright 2019 Manna Harbour
 
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifdef OLED_ENABLE
+#include <stdio.h>
+#include <luna.h>
+#include <spaceship.h>
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (!is_keyboard_master()) {
+    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+  }
+  return rotation;
+}
+
+static void print_status_narrow(void) {
+    /* Print current layer */
+    oled_write(get_highest_layer(layer_state), false);
+
+    /* KEYBOARD PET RENDER START */
+
+    render_luna(0, 13);
+
+    /* KEYBOARD PET RENDER END */
+}
+
+
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        render_space();
+    }
+    return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+  return true;
+}
+#endif // OLED_ENABLE
---
+++ keyboards/crkbd/keymaps/manna-harbour_miryoku/luna.h
@@ -0,0 +1,6 @@
+#ifndef spaceship_h
+#define spaceship_h
+
+extern void render_luna(void);
+
+#endif
